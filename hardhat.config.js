4fe99dd23745bb9d5c04c12840708ccfef421802
Kensuke Tanaka <6182830+tanakaoriginal@users.noreply.github.com>
2021-11-25 23:50:39 +0900

    Add deploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,15 @@ require("@nomiclabs/hardhat-waffle");
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    ganache: {
+      url: "http://ganache:8545"
+    }
+  },
   solidity: {
     version: "0.8.9",
     settings: {
