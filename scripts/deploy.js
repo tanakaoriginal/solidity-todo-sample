f5add7b216555777082b6817439bf4c9b58ebe10
Kensuke Tanaka <6182830+tanakaoriginal@users.noreply.github.com>
2021-11-26 12:11:29 +0900

    Add contract file generator for frontend

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -11,6 +11,29 @@ async function main() {
   const todoList = await TodoList.deploy();
 
   console.log("Todo list count:", await todoList.getItemCount());
+
+  saveFrontendFiles(todoList);
+}
+
+function saveFrontendFiles(todoList) {
+  const fs = require("fs");
+  const contractsDir = __dirname + "/../frontend/src/contracts";
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  fs.writeFileSync(
+    contractsDir + "/contract-address.json",
+    JSON.stringify({ TodoList: todoList.address }, undefined, 2)
+  );
+
+  const TokenArtifact = artifacts.readArtifactSync("TodoList");
+
+  fs.writeFileSync(
+    contractsDir + "/TodoList.json",
+    JSON.stringify(TokenArtifact, null, 2)
+  );
 }
 
 main()
